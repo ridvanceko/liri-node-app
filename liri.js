33af7500c8ed14177a97fefed50e8fef79959d55
Ridvan Cekmez <ridvanceko@gmail.com>
2019-10-09 15:54:38 -0700

    configured four conditions structure

--- liri.js
+++ liri.js
@@ -1,5 +1,36 @@
 require("dotenv").config();
 
-var keys = require("./key");
+let keys = require("./key");
+let spotify = new Spotify(keys.Spotify);
+let command = new Command(process.argv[2], process.argv[3]);
+command.run();
 
-var spotify = new Spotify(keys.spotify);
+function Spotify(keys) {
+   this.newId = keys.id;
+   this.newSecret = keys.secret;
+};
+
+
+
+function Command(command, option) {
+  this.newCommand = command;
+  this.newOption = option;
+
+  this.run = function() {
+      if(this.newCommand === "concert-this"){
+        console.log("This is concert-this command")
+      }
+      else if(this.newCommand === "spotify-this-song"){
+        console.log("This spotify-this-song command")
+      }
+      else if(this.newCommand === "movie-this"){
+        console.log("This is movie-this")
+      }
+      else if(this.newCommand === "do-what-it-say") {
+        console.log("This is do-what-it-say")
+      }
+      
+  }
+
+
+}
