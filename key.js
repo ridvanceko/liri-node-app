da6a4916a83c94234783091ec1fbfaf2af36d040
Ridvan Cekmez <ridvanceko@gmail.com>
2019-10-15 18:20:30 -0700

    movie-this responsive

--- key.js
+++ key.js
@@ -5,3 +5,9 @@ exports.Spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.OMDB = {
+
+ key: process.env.OMDB,
+
+}
--- liri.js
+++ liri.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const axios = require("axios");
 const moment = require("moment");
 
-let keys = require("./key");
+let keys = require("./key.js");
 let spotify = new Spotify(keys.Spotify);
+let omdb = new OMDB(keys);
 let command = new Command(process.argv[2], process.argv[3]);
+
 command.run();
 
 function Spotify(keys) {
@@ -13,10 +15,10 @@ function Spotify(keys) {
 };
 
 
-
 function Command(command, option) {
   this.newCommand = command;
   this.newOption = option;
+  
 
   this.run = function () {
     if (this.newCommand === "concert-this") {
@@ -28,7 +30,7 @@ function Command(command, option) {
       // Make a request for a user with a given ID
       axios.get(URL)
         .then(function (response) {
-          // handle success
+          
 
           response.data.forEach((info) => {
             console.log("Name: " + info.venue.name);
@@ -44,56 +46,59 @@ function Command(command, option) {
           console.log(error);
         })
     }
-
-this.run = function() {
-  if (this.newSong === "spotify-this-song") {
-    console.log("This is spotify-this-song song")
-
-    let URL = "https://www.npmjs.com/package/node-spotify-api" + this.newOption + "/events?app_id=codingbootcamp"
-
-
-      // Make a request for a user with a given ID
-      axios.get(URL)
-        .then(function (response){
-          console.log('      Artist: ' + track['0'].artists['0'].name);
-          console.log('The song\'s Name: ' + track['0'].name);
-          console.log('   Spotify Link: ' + track['0'].preview_url);
-          console.log('          Album: ' + track['0'].album.name + '\n');
-        }
-        
-        .catch(function (error) {
-          // handle error
-          console.log(error);
-        })
-
-  }
-
-
-
-   
-
-
-
-    
-   
-
-
-    else if (this.newCommand === "spotify-this-song") {
-      console.log("This spotify-this-song command")
+ else if (this.newCommand === "spotify-this-song") {
+      console.log("This spotify-this-song command");
     }
     else if (this.newCommand === "movie-this") {
-      console.log("This is movie-this")
-    }
-    else if (this.newCommand === "do-what-it-say") {
-      console.log("This is do-what-it-say")
-    }
+      console.log("This is movie-this");
 
+      
+      let URL = "http://www.omdbapi.com/?t="+option.split("-")[0]+"&y="+option.split("-")[1]+"&apikey=" + omdb.getKey();
+      axios.get(URL)
+          .then(function (response) {
+              // handle success
+              let info = response.data;
+                  this._string = "Name: "+info.Title+"\n" +
+                      "Year: "+info.Year+"\n" +
+                      "Rated: "+info.Rated+"\n" +
+                      "Released: "+info.Released+"\n" +
+                      "Runtimr: "+info.Runtime+"\n" +
+                      "Genre: "+info.Genre+"\n" +
+                      "Director: "+info.Director+"\n" +
+                      "Actors: "+info.Director+"\n" +
+                      "Languages: "+info.Language+"\n";
+              console.log(this._string);
+          })
+          .catch(function (error) {
+              // handle error
+              console.log(error);
+          });
 
+  }else if(this._command === "do-what-it-says"){
+      console.log(this._printCommand);
   }
-
+};
 
 }
 
+function Spotify(keys) {
+this._ID = keys.id;
+this._secret = keys.secret;
 
+this.getId = () => {
+  return this._ID;
+};
 
+this.getSecret = () => {
+  return this._secret;
+}
+}
+
+function OMDB(keys) {
+this._key = keys.OMDB.key;
+
+this.getKey = () =>{
+  return this._key
+}
+}
 
